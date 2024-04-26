0811cc0e4c9d0bfe3e7f24633a819e0fd4c9f94e
sudeepgtm777 <sudeepgautam777@gmail.com>
2024-04-26 18:54:23 +0545

    Adding routes and listening to server in port 3000

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+app.get('/home', (req, res) => {
+  res.json('You are on Home page');
+});
+
+app.get('/course/grades', (req, res) => {
+  res.json({
+    Shyam: 70,
+    Brandy: 80,
+    Naresh: 40,
+    Suresh: 55,
+  });
+});
+
+app.get('/course/:number', (req, res) => {
+  const courseNumber = req.params.number;
+  res.json(`'You have the permission to see the course ${courseNumber}'`);
+});
+
+app.listen(3000, () => {
+  console.log('Server running on port 3000');
+});
